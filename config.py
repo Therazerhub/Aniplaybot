5f0c1c5e2d3b5b82cec54a99a742372ad75d1ba3
Razer <therazerhub@gmail.com>
2024-11-06 11:14:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # TOKEN = '4213543775:AsfjgfgdsdhfsdjhgjhfgsjgY'
 
 
-API_ID = ''
-API_HASH = ''
-TOKEN = ''
+API_ID = '20227614'
+API_HASH = 'b9268f7d01884a3d740ba17d0ad8754b'
+TOKEN = '6512034283:AAHt9zcnAXvQHYD-kYVJrhfrbpxjE5kSHhE'
